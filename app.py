21259e48175e51d969bf4d1a11f6fd61d66170f9
Kesavan Chinnasamy <kesavanchinnasamy92@gmail.com>
2024-05-14 18:53:29 +0530

    Share info app first push

--- app.py
+++ app.py
@@ -5,15 +5,61 @@ import csv
 
 class Protfolio:
     def __init__(self):
-        pass
+        self.head = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/87.0.4280.88 Safari/537.36 "}
+        self.get_details = 'https://www.nseindia.com/api/quote-equity?symbol={}'
+        
 
     def get_info(self):
+
+        current_data = []
         
+        # read the current protfolio from csv file
         with open('holdings.csv', 'r') as file:
             csv_reader = csv.reader(file, delimiter=',')
-            print("Temp")
+            
+            for i in csv_reader:
+                share_dict = {}
+                if i[0] == 'Instrument':
+                    pass
+                else:
+                    share_dict['stock_name'] = i[0]
+                    share_dict['quantity'] = int(i[1])
+                    share_dict['price'] = float(i[2])
+                    share_dict['invested_value'] = int(i[1]) * float(i[2])
+                
+                    current_data.append(share_dict)
+        session = requests.session()
+        session.get('https://www.nseindia.com/', headers=self.head)
 
-            print(csv_reader)
+        total_data = []
+        for stock in current_data:
+            # print(stock)
+            stock_name = stock['stock_name']
+            
+            try:
+                url = str(self.get_details.format(stock_name))
+                company_details = session.get(url=url, headers=self.head)
+                live_data = company_details.json()
+
+                stock['eod_price'] = float(live_data['priceInfo']['lastPrice'])
+                stock['52_week_high'] = float(live_data['priceInfo']['weekHighLow']['max'])
+                stock['52_week_low'] = float(live_data['priceInfo']['weekHighLow']['min'])
+                stock['current_value'] = stock['quantity'] * stock['eod_price']
+                stock['profit/loss'] = (stock['current_value'] - stock['invested_value'])/100
+                    
+                total_data.append(stock)
+
+            except Exception as err:
+                print(err)
+                print()
+
+        print(total_data)
+
+
+
+
+                
 
 obj1 = Protfolio()
 obj1.get_info()
